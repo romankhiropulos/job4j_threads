c3a6b2c276245f602aa2c0d08ec2592dcf27e241
romankhiropulos <romankhiropulos@gmail.com>
2021-12-03 11:34:36 +0300

    add test method for main method

--- src/main/java/ru/job4j/threads/Main.java
+++ src/main/java/ru/job4j/threads/Main.java
@@ -1,4 +1,7 @@
 package ru.job4j.threads;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/ru/job4j/threads/MainTest.java
+++ src/test/java/ru/job4j/threads/MainTest.java
@@ -1,7 +1,12 @@
 package ru.job4j.threads;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
 
+    @Test
+    public void main() {
+    }
 }
