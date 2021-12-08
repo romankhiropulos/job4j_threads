e0b342d064c6f34a6a94a1742f001a69777f19ca
romankhiropulos <romankhiropulos@gmail.com>
2021-12-08 15:01:55 +0300

    Thread state while with sout

--- src/main/java/ru/job4j/threads/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/threads/concurrent/ThreadState.java
@@ -3,20 +3,17 @@ package ru.job4j.threads.concurrent;
 public class ThreadState {
 
     public static void main(String[] args) {
-        Thread first = new Thread(() -> printCurrentThreadName(50), "First thread");
-        Thread second = new Thread(() -> printCurrentThreadName(50), "Second thread");
+        Thread first = new Thread(() -> printCurrentThreadName(1000), "First thread");
+        Thread second = new Thread(() -> printCurrentThreadName(1000), "Second thread");
         System.out.println(first.getState());
         System.out.println(second.getState());
         first.start();
         second.start();
-        System.out.println(first.getState());
-        System.out.println(second.getState());
-        printCurrentThreadName(20);
-
-        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
-            continue;
+        while (!(first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED)) {
+            System.out.println(
+                    "First thread state: " + first.getState() + "; Second thread state: " + second.getState()
+            );
         }
-        System.out.println("First thread state: " + first.getState() + "; Second thread state: " + second.getState());
         System.out.println("Работа завершена!");
     }
 
