ead9ef75a15220779cd8dfbe8eb06f9da68a3f47
romankhiropulos <romankhiropulos@gmail.com>
2021-12-08 21:33:19 +0300

    cache non atomic

---
+++ src/main/java/ru/job4j/threads/commonresources/Cache.java
@@ -0,0 +1,20 @@
+package ru.job4j.threads.commonresources;
+
+public final class Cache {
+
+    private static Cache cache;
+
+    public static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(() -> System.out.println(instOf() + " from threadFirst"));
+        Thread second = new Thread(() -> System.out.println(instOf() + " from threadSecond"));
+        first.start();
+        second.start();
+    }
+}
