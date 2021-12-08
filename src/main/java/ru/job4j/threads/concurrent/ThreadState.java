2c738938d3df4f07ce269d416b0faee5dfb294d9
romankhiropulos <romankhiropulos@gmail.com>
2021-12-08 20:29:24 +0300

    thread state while or

--- src/main/java/ru/job4j/threads/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/threads/concurrent/ThreadState.java
@@ -9,7 +9,7 @@ public class ThreadState {
         System.out.println(second.getState());
         first.start();
         second.start();
-        while (!(first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED)) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
             System.out.println(
                     "First thread state: " + first.getState() + "; Second thread state: " + second.getState()
             );
