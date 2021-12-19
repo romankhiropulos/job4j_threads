12f934faee3824dece053df9f17045e9a332b361
romankhiropulos <romankhiropulos@gmail.com>
2021-12-19 18:31:26 +0300

    threadsafe UserCache

---
+++ src/main/java/ru/job4j/threads/commonresources/UserCache.java
@@ -0,0 +1,26 @@
+package ru.job4j.threads.commonresources;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public final class UserCache {
+
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(users.values()).stream().map(u -> User.of(u.getName())).collect(Collectors.toList());
+    }
+}
