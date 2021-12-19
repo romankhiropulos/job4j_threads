b0295bd9d291dc2f118ebdf136347248e279831f
romankhiropulos <romankhiropulos@gmail.com>
2021-12-19 18:04:23 +0300

    immutable node

---
+++ src/main/java/ru/job4j/threads/commonresources/Node.java
@@ -0,0 +1,21 @@
+package ru.job4j.threads.commonresources;
+
+public final class Node<T> {
+
+    private final Node<T> next;
+
+    private final T value;
+
+    public Node(final Node<T> next, final T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
