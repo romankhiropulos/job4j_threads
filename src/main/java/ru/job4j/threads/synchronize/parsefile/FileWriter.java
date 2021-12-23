7278b2d295944abc2e630003640ca23c583c41e7
romankhiropulos <romankhiropulos@gmail.com>
2021-12-23 20:29:00 +0300

    2. JCIP. Настройка библиотеки [#268575]

--- src/main/java/ru/job4j/threads/synchronize/parsefile/FileWriter.java
+++ src/main/java/ru/job4j/threads/synchronize/parsefile/FileWriter.java
@@ -1,13 +1,18 @@
 package ru.job4j.threads.synchronize.parsefile;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
+@ThreadSafe
 public final class FileWriter {
 
+    @GuardedBy("this")
     private final File file;
 
     public FileWriter(File file) {
@@ -15,7 +20,7 @@ public final class FileWriter {
     }
 
     public synchronized void saveContent(String content) throws IOException {
-        try (OutputStream outputStream = new FileOutputStream(file)) {
+        try (OutputStream outputStream = new FileOutputStream(this.file)) {
             outputStream.write(content.getBytes(StandardCharsets.UTF_8));
         }
     }
---
+++ src/test/java/ru/job4j/threads/synchronize/count/CountTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.threads.synchronize.count;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CountTest {
+
+    /**
+     * Класс описывает нить со счетчиком.
+     */
+    private static class ThreadCount extends Thread {
+
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count();
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get(), is(2));
+
+    }
+}
