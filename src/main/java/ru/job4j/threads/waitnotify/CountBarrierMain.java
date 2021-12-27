a18c491c7da506a9e173aa85c5d448cf842725ea
romankhiropulos <romankhiropulos@gmail.com>
2021-12-27 19:54:27 +0300

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/threads/waitnotify/CountBarrierMain.java
@@ -0,0 +1,27 @@
+package ru.job4j.threads.waitnotify;
+
+public class CountBarrierMain {
+
+    public static void main(String[] args) {
+
+        CountBarrier barrier = new CountBarrier(100);
+        Thread counter = new Thread(barrier::count);
+        Thread await1 = new Thread(
+                () -> {
+                    barrier.await();
+                    System.out.println(Thread.currentThread().getName());
+                },
+                "await1"
+        );
+        Thread await2 = new Thread(
+                () -> {
+                    barrier.await();
+                    System.out.println(Thread.currentThread().getName());
+                },
+                "await2"
+        );
+        await1.start();
+        await2.start();
+        counter.start();
+    }
+}
