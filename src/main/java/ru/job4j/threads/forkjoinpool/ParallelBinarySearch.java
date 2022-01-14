f7371199d9f03072ca5849197fe2b295072a80bd
romankhiropulos <romankhiropulos@gmail.com>
2022-01-14 11:59:46 +0300

    3. ForkJoinPool [#315067] add static sort()

--- src/main/java/ru/job4j/threads/forkjoinpool/ParallelBinarySearch.java
+++ src/main/java/ru/job4j/threads/forkjoinpool/ParallelBinarySearch.java
@@ -38,8 +38,8 @@ public class ParallelBinarySearch<T extends Comparable<T>> extends RecursiveTask
         return leftIndex != -1 ? leftIndex : rightIndex;
     }
 
-    int linearSearch() {
-        for (int i = 0; i <= array.length; i++) {
+    private int linearSearch() {
+        for (int i = startIndex; i <= endIndex; i++) {
             if (array[i].equals(key)) {
                 return i;
             }
@@ -47,14 +47,8 @@ public class ParallelBinarySearch<T extends Comparable<T>> extends RecursiveTask
         return -1;
     }
 
-    public static void main(String[] args) {
-        Integer[] array = new Integer[50];
-        for (int i = 0; i < 50; i++) {
-            array[i] = i;
-        }
+    public static <T extends Comparable<? super T>> int sort(T[] array, T key) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        ParallelBinarySearch<Integer> searcher = new ParallelBinarySearch<>(array, 44, 0, 49);
-        int keyPosition = forkJoinPool.invoke(searcher);
-        System.out.println("keyPosition " + keyPosition);
+        return (int) forkJoinPool.invoke(new ParallelBinarySearch(array, key, 0, array.length - 1));
     }
 }
