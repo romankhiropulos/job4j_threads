31b0535a9b95f92c3e62ebf034c279fc6864d660
romankhiropulos <romankhiropulos@gmail.com>
2022-01-20 21:24:52 +0300

    2. Модель памяти Java [#267917] DCLSingleton

---
+++ src/main/java/ru/job4j/threads/commonresources/DCLSingleton.java
@@ -0,0 +1,21 @@
+package ru.job4j.threads.commonresources;
+
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
